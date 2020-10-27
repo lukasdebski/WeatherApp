fc6ca95b3d899ae9d00a4e0f8cb769c9ebf7bf17
lukasdebski <lukasdebski87@gmail.com>
2020-10-27 10:54:53 +0100

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Header';
+
+    const Header = () => {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Hello, World!</h1>
+                </header>
+            </div>
+        )
+    }
+
+
+export default Header;
