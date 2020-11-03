c074e182a38ae171499ec6190479a40e4e11b8ec
lukasdebski <lukasdebski87@gmail.com>
2020-11-03 17:59:17 +0100

    add view for tablet and desktop

--- src/components/Body/Result.js
+++ src/components/Body/Result.js
@@ -19,36 +19,36 @@ const Result = props => {
         const iconWeather = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
         content = (
-            <div className='result__container'>
+            <section className='result__container'>
 
-                <div className="result__element">
+                <article className="result__element">
                     <div className='result__el el--info'>Informacje pogodowe dla:</div>
                     <div className='result__el el--city'>{city}</div>
-                </div>
+                </article>
 
-                <div className="result__element">
+                <article className="result__element elements--temp">
                     <div className='result__el el--temp'>{tempRound} &#176;C</div>
-                    <div className='result__el el--info'>odczuwalna: {feelsRound} &#176;C</div>
+                    <div className='result__el el--info'>Odczuwalna: {feelsRound} &#176;C</div>
                     <div className="result__box">
                         <div className='result__el box--el'>min: {tempMinRound} &#176;C</div>
                         <div className='result__el box--el'>max: {tempMaxRound} &#176;C</div>
                     </div>
                     <div className='result__element'>
                         <img className='result__el icon--el' src={iconWeather} alt=''/>
-                        <div className='result__el'>{description}</div>
+                        <div className='result__el description'>{description}</div>
                     </div>
-                </div>
+                </article>
 
-                <div className="result__elements">
+                <article className="result__elements">
                     <div className="result__box">
                         <div className='result__el el--sun'><i className="fas fa-sort-up"></i><i className="far fa-sun"></i> {sunriseTime}</div>
                         <div className='result__el el--sun'><i className="far fa-sun"></i><i className="fas fa-sort-down"></i> {sunsetTime}</div>
                     </div>
-                    <div className='result__el el--sun'>ciśnienie: {pressure} hPa</div>
-                    <div className="result__el el--sun">wilgotność: {humidity} %</div>
+                    <div className='result__el el--sun'>Ciśnienie: {pressure} hPa</div>
+                    <div className="result__el el--sun">Wilgotność: {humidity} %</div>
                     <div className='result__el el--wind el--sun'><i className="fas fa-wind"></i> {wind} m/s</div>
-                </div>
-            </div>
+                </article>
+            </section>
         )
     }
 
